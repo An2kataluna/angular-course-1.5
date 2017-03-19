d96682b80e05a868e5f1db46b8fc1a63a68b636f
Ani Krivova <anjelika.k08@gmail.com>
2017-03-19 19:49:43 +0200

    Minimal Changes for personal purposes

--- src/app/constants/app.constants.js
+++ src/app/constants/app.constants.js
@@ -1,6 +1,6 @@
 import 'angular';
 
 export default angular.module('app.constants',[])
-    .constant('API_URL','http://57e0fb4e4ed1d8110064d494.mockapi.io/api/v1/')
+    .constant('API_URL','http://58be88354389c312007f4030.mockapi.io/api/v1/') /* http://57e0fb4e4ed1d8110064d494.mockapi.io/api/v1/ */
     .constant('SESSION_TIME', 600000)
     .name;
